1c6b98c0ad398351298a29acb563a31fa87f4e1e
mari1811 <40413045+mari1811@users.noreply.github.com>
2019-01-10 23:01:40 -0400

    Update servidor.js

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -89,10 +89,10 @@ app.get('/orden', (req, res) =>{
     cliente.connect()
       .then(() => {
 
-        const sqlOrden = 'DELETE * FROM orden WHERE idOrdenRecoleccion = $1;'
-        //const parametrosOrden = [req.body.id_orden]
+        const sqlOrden = 'DELETE FROM orden WHERE idOrdenRecoleccion = $1;'
+        const parametrosOrden = [req.body.id_orden]
         var resultado = {
-          //orden:cliente.query(sqlOrden, parametrosOrden)
+         orden:cliente.query(sqlOrden, parametrosOrden)
         }
         return resultado
       })
